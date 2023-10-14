88ef82510a02284587e8a39c67c132e416e38e97
Kostandinos Myrtollari <sdi2000148@di.uoa.gr>
2023-10-14 23:19:07 +0300

    fixed brute force solution

--- Algorithms/brute_force.c
+++ Algorithms/brute_force.c
@@ -5,21 +5,11 @@
 
 int brute_force(Dataset dataset, int k, Metric metric)
 {
-    int count;
-
-    for (int i=0 ; i < dataset->numberOfObjects ; i++){
-        
+    for (int i=0 ; i < dataset->numberOfObjects ; i++){       
         heap_initialize(&dataset->objects[i]->heap, k);
-        count = 0;
         for (int j=0 ; j < dataset->numberOfObjects ; j++){
             if (j == i) continue;
-            if (count < k) {
-                count++;
-                heap_update(dataset->objects[i]->heap, j, metric(dataset->objects[i]->features, dataset->objects[j]->features, dataset->dimensions));
-            }
-            else {
-                heap_update(dataset->objects[i]->heap, j, metric(dataset->objects[i]->features, dataset->objects[j]->features, dataset->dimensions));
-            }
+            heap_update(dataset->objects[i]->heap, j, metric(dataset->objects[i]->features, dataset->objects[j]->features, dataset->dimensions));
         }
     }
     return 0;
