a39c18e591cfeeebcdfaef3202d6e6357a3ddc5e
Aggelos Gkounelas <aggelosgounelas27@gmail.com>
2023-11-05 13:47:29 +0200

    tests services update

--- Tests/services_tests.c
+++ Tests/services_tests.c
@@ -23,35 +23,15 @@ void test_nn_update(void) {
     nn_update(heaps, 4, 2, 17.4, R);
     nn_update(heaps, 4, 1, 19.4, R);
 
-    TEST_CHECK(avl_search(R[0], 0) == 0);
-    TEST_CHECK(avl_search(R[0], 1) == 0);
-    TEST_CHECK(avl_search(R[0], 2) == 0);
-    TEST_CHECK(avl_search(R[0], 3) == 0);
-    TEST_CHECK(avl_search(R[0], 4) == 0);
-
-    TEST_CHECK(avl_search(R[1], 0) == 0);
-    TEST_CHECK(avl_search(R[1], 1) == 0);
-    TEST_CHECK(avl_search(R[1], 2) == 1);
-    TEST_CHECK(avl_search(R[1], 3) == 0);
-    TEST_CHECK(avl_search(R[1], 4) == 0);
-
-    TEST_CHECK(avl_search(R[2], 0) == 0);
-    TEST_CHECK(avl_search(R[2], 1) == 0);
-    TEST_CHECK(avl_search(R[2], 2) == 0);
-    TEST_CHECK(avl_search(R[2], 3) == 1);
-    TEST_CHECK(avl_search(R[2], 4) == 0);
-
-    TEST_CHECK(avl_search(R[3], 0) == 0);
-    TEST_CHECK(avl_search(R[3], 1) == 0);
-    TEST_CHECK(avl_search(R[3], 2) == 0);
-    TEST_CHECK(avl_search(R[3], 3) == 0);
-    TEST_CHECK(avl_search(R[3], 4) == 0);
-
-    TEST_CHECK(avl_search(R[4], 0) == 1);
-    TEST_CHECK(avl_search(R[4], 1) == 0);
-    TEST_CHECK(avl_search(R[4], 2) == 0);
-    TEST_CHECK(avl_search(R[4], 3) == 1);
-    TEST_CHECK(avl_search(R[4], 4) == 0);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 1 && j == 2) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 2 && j == 3) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 0) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 3) TEST_CHECK(avl_search(R[i], j) == 1);
+            else TEST_CHECK(avl_search(R[i], j) == 0);
+        }
+    }
 
     //Η περιπτωση οπου ο κομβος 4 που βγαινει έχει ακμη προς τον κομβο 2 που αλλαζουμε το heap του
     //αρα πρεπει να προσθεθει ο 4 στους reserves του 2
@@ -59,35 +39,15 @@ void test_nn_update(void) {
     //αρα θα πρεπει να αφαιρεθει απο τους reverses του 2  
     nn_update(heaps, 2, 3, 9.4, R);
 
-    TEST_CHECK(avl_search(R[0], 0) == 0);
-    TEST_CHECK(avl_search(R[0], 1) == 0);
-    TEST_CHECK(avl_search(R[0], 2) == 0);
-    TEST_CHECK(avl_search(R[0], 3) == 0);
-    TEST_CHECK(avl_search(R[0], 4) == 0);
-
-    TEST_CHECK(avl_search(R[1], 0) == 0);
-    TEST_CHECK(avl_search(R[1], 1) == 0);
-    TEST_CHECK(avl_search(R[1], 2) == 1);
-    TEST_CHECK(avl_search(R[1], 3) == 0);
-    TEST_CHECK(avl_search(R[1], 4) == 0);
-
-    TEST_CHECK(avl_search(R[2], 0) == 0);
-    TEST_CHECK(avl_search(R[2], 1) == 0);
-    TEST_CHECK(avl_search(R[2], 2) == 0);
-    TEST_CHECK(avl_search(R[2], 3) == 0);
-    TEST_CHECK(avl_search(R[2], 4) == 1);
-
-    TEST_CHECK(avl_search(R[3], 0) == 0);
-    TEST_CHECK(avl_search(R[3], 1) == 0);
-    TEST_CHECK(avl_search(R[3], 2) == 0);
-    TEST_CHECK(avl_search(R[3], 3) == 0);
-    TEST_CHECK(avl_search(R[3], 4) == 0);
-
-    TEST_CHECK(avl_search(R[4], 0) == 1);
-    TEST_CHECK(avl_search(R[4], 1) == 0);
-    TEST_CHECK(avl_search(R[4], 2) == 0);
-    TEST_CHECK(avl_search(R[4], 3) == 1);
-    TEST_CHECK(avl_search(R[4], 4) == 0);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 1 && j == 2) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 2 && j == 4) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 0) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 3) TEST_CHECK(avl_search(R[i], j) == 1);
+            else TEST_CHECK(avl_search(R[i], j) == 0);
+        }
+    }
 
     //Η περιπτωση οπου ο κομβος 1 που βγαινει έχει ακμη προς τον κομβο 0 που αλλαζουμε το heap του
     //αρα πρεπει να προσθεθει ο 1 στους reserves του 0
@@ -95,36 +55,17 @@ void test_nn_update(void) {
     //αρα θα πρεπει να προσθεθει στους reverses του ο 0 
     nn_update(heaps, 0, 3, 9.4, R);
 
-
-    TEST_CHECK(avl_search(R[0], 0) == 0);
-    TEST_CHECK(avl_search(R[0], 1) == 1);
-    TEST_CHECK(avl_search(R[0], 2) == 0);
-    TEST_CHECK(avl_search(R[0], 3) == 0);
-    TEST_CHECK(avl_search(R[0], 4) == 0);
-
-    TEST_CHECK(avl_search(R[1], 0) == 0);
-    TEST_CHECK(avl_search(R[1], 1) == 0);
-    TEST_CHECK(avl_search(R[1], 2) == 1);
-    TEST_CHECK(avl_search(R[1], 3) == 0);
-    TEST_CHECK(avl_search(R[1], 4) == 0);
-
-    TEST_CHECK(avl_search(R[2], 0) == 0);
-    TEST_CHECK(avl_search(R[2], 1) == 0);
-    TEST_CHECK(avl_search(R[2], 2) == 0);
-    TEST_CHECK(avl_search(R[2], 3) == 0);
-    TEST_CHECK(avl_search(R[2], 4) == 1);
-
-    TEST_CHECK(avl_search(R[3], 0) == 1);
-    TEST_CHECK(avl_search(R[3], 1) == 0);
-    TEST_CHECK(avl_search(R[3], 2) == 0);
-    TEST_CHECK(avl_search(R[3], 3) == 0);
-    TEST_CHECK(avl_search(R[3], 4) == 0);
-
-    TEST_CHECK(avl_search(R[4], 0) == 1);
-    TEST_CHECK(avl_search(R[4], 1) == 0);
-    TEST_CHECK(avl_search(R[4], 2) == 0);
-    TEST_CHECK(avl_search(R[4], 3) == 1);
-    TEST_CHECK(avl_search(R[4], 4) == 0);
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0 && j == 1) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 1 && j == 2) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 2 && j == 4) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 3 && j == 0) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 0) TEST_CHECK(avl_search(R[i], j) == 1);
+            else if (i == 4 && j == 3) TEST_CHECK(avl_search(R[i], j) == 1);
+            else TEST_CHECK(avl_search(R[i], j) == 0);
+        }
+    }
 
     for (int i = 0; i < 5; i++) {
         avl_free(R[i]);
