f3b29ffb68f4ebc002dc9f24631aa0ec65245a3c
Kostandinos Myrtollari <sdi2000148@di.uoa.gr>
2023-12-04 20:58:15 +0200

    refined scripts

--- run_brute.py
+++ run_brute.py
@@ -1,12 +1,14 @@
 import subprocess
 import os
 
-# create csv file
 results_dir = 'BruteResults/'
 dataset_dir = 'Datasets/'
 solution_dir = 'Solutions/'
 
-# preparing arguments list
+if not os.path.exists(dataset_dir):
+    print(f"Directory '{dataset_dir}' does not exist!")
+    exit(0)
+
 args = []
 outputs = []
 files_list = os.listdir(dataset_dir)
@@ -16,16 +18,20 @@ for file in files_list:
         type = 1
     elif part2 == 'ascii':
         continue
+    output_path = results_dir + part1 + '.csv'
     for k in [100]:
+        solution_path = solution_dir + part1 + '-' + str(k) + '.txt'
+        if os.path.exists(solution_path):
+            print(f"The file '{solution_path}' already exists.")
+            continue
+        if output_path not in outputs:
+            outputs.append(output_path)
         for metric in ['l2']:
-            temp = solution_dir + part1 + '-' + str(k) + '.txt'
-            if os.path.exists(temp):
-                print(f"The file '{temp}' already exists.")
-                continue
-            output = part1 + '.csv'
-            if output not in outputs:
-                outputs.append(output)
-            args += [[output, type, dataset_dir+file, temp, k, metric]]
+            args += [[output_path, type, dataset_dir+file, solution_path, k, metric]]
+
+if args == []:
+    print('Nothing to run. Exiting.')
+    exit(0)
 
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
@@ -40,18 +46,18 @@ else:
     print(f"Directory '{solution_dir}' already exists.")
 
 for output in outputs:
-    filename = results_dir + output
-    with open(filename, 'w') as file:
+    with open(output, 'w') as file:
         file.write('Type,Dataset,N,k,Metric,Time\n')
 
 # run multiple experiments
 subprocess.run(['make', 'brute'])
 
-for filename, type, dataset, solution, k, metric in args:
-    print('./brute', str(type), dataset, metric, str(k), solution, results_dir+filename)
-    subprocess.run(['./brute', str(type), dataset, metric, str(k), solution, results_dir+filename])
+for output, type, dataset, solution, k, metric in args:
+    print('./brute', str(type), dataset, metric, str(k), solution, output)
+    subprocess.run(['./brute', str(type), dataset, metric, str(k), solution, output])
 
 subprocess.run(['make', 'clean-brute'])
+
 
 
 
--- run_nn.py
+++ run_nn.py
@@ -1,12 +1,18 @@
 import subprocess
 import os
 
-# create csv file
 results_dir = 'NNResults/'
 dataset_dir = 'Datasets/'
 solution_dir = 'Solutions/'
 
-# preparing arguments list
+if not os.path.exists(dataset_dir):
+    print(f"Directory '{dataset_dir}' does not exist!")
+    exit(0)
+
+if not os.path.exists(solution_dir):
+    print(f"Directory '{solution_dir}' does not exist!")
+    exit(0)
+
 args = []
 outputs = []
 files_list = os.listdir(dataset_dir)
@@ -16,16 +22,21 @@ for file in files_list:
         type = 1
     elif part2 == 'ascii':
         continue
+    output_path = results_dir + part1 + '.csv'
+    if os.path.exists(output_path):
+        print(f"The file '{output_path}' already exists.")
+        continue
+    outputs.append(output_path)
     for k in [100]:
+        solution_path = solution_dir + part1 + '-' + str(k) + '.txt'
         for p in [0.2, 0.4, 1]:
             for d in [0.001]:
                 for metric in ['l2']:
-                    temp = solution_dir + part1 + '-' + str(k) + '.txt'
-                    output = part1 + '.csv'
-                    if output not in outputs:
-                        outputs.append(output)
-                    args += [[output, type, dataset_dir+file, temp, k, p, d, metric]]
+                    args += [[output_path, type, dataset_dir+file, solution_path, k, p, d, metric]]
 
+if args == []:
+    print('All experiments already conducted. Exiting.')
+    exit(0)
 
 if not os.path.exists(results_dir):
     os.makedirs(results_dir)
@@ -34,16 +45,15 @@ else:
     print(f"Directory '{results_dir}' already exists.")
 
 for output in outputs:
-    filename = results_dir + output
-    with open(filename, 'w') as file:
+    with open(output, 'w') as file:
         file.write('Type,Dataset,N,k,Recall,Metric,p,d,Time\n')
 
 # run multiple experiments
 subprocess.run(['make', 'nn'])
 
-for filename, type, dataset, solution, k, p, d, metric in args:
-    print('./nn', str(type), dataset, metric, str(k), str(p), str(d), solution, results_dir+filename)
-    subprocess.run(['./nn', str(type), dataset, metric, str(k), str(p), str(d), solution, results_dir+filename])
+for output, type, dataset, solution, k, p, d, metric in args:
+    print('./nn', str(type), dataset, metric, str(k), str(p), str(d), solution, output)
+    subprocess.run(['./nn', str(type), dataset, metric, str(k), str(p), str(d), solution, output])
 
 subprocess.run(['make', 'clean-nn'])
 
